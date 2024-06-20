144ae995c66d75097af8e54b15681c072ad6f49d
Chandranathmishra <chandranathmishra881@gmail.com>
2024-06-20 14:07:41 +0530

    build

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -3,10 +3,11 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "./", // Ensures relative paths for assets
   server: {
     proxy: {
       "/api": {
-        target: "htp://localhost:4000",
+        target: "http://localhost:4000", // Corrected 'http' instead of 'htp'
         secure: false,
       },
     },
